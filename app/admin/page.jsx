09c5b915e8c3ad4a5ec9e28e373e6685b9abd63a
Sviat Petryshyn <85065470+SviatF@users.noreply.github.com>
2026-09-21 06:41:15 -0700

    Add COSMO manager admin route

---
+++ app/admin/page.jsx
@@ -0,0 +1,7 @@
+import AdminApplications from '../AdminApplications';
+
+export const metadata = { title: 'COSMO Manager Review', robots: { index: false, follow: false } };
+
+export default function AdminPage() {
+  return <AdminApplications />;
+}
